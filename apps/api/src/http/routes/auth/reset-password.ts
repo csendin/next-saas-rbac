f83ea5984e612a4278477412525b3a73489a4561
csendin <caiosendindev@gmail.com>
2025-01-26 17:26:13 -0300

    feat: create password change route

---
+++ apps/api/src/http/routes/auth/reset-password.ts
@@ -0,0 +1,46 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { UnauthorizedError } from '@/http/errors/unauthorized-error'
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
+
+export async function resetPassword(app: FastifyInstance) {
+    app.withTypeProvider<ZodTypeProvider>().post(
+        '/password/reset',
+        {
+            schema: {
+                tags: ['auth'],
+                summary: `Reset the user's password`,
+                body: z.object({
+                    code: z.string(),
+                    password: z.string().min(6),
+                }),
+                response: {
+                    204: z.null(),
+                },
+            },
+        },
+        async (request, reply) => {
+            const { code, password } = request.body
+
+            const token = await prisma.token.findUnique({
+                where: { id: code },
+            })
+
+            if (!token) {
+                throw new UnauthorizedError()
+            }
+
+            const hashedPassword = await hash(password, 6)
+
+            await prisma.user.update({
+                where: { id: token.userId },
+                data: { password: hashedPassword },
+            })
+
+            return reply.status(204).send()
+        }
+    )
+}
