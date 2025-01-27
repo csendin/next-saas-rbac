6e875dff30dbb210121e3aab4971960a7857fd44
csendin <caiosendindev@gmail.com>
2025-01-27 19:06:28 -0300

    feat: add listing routes and organization detail

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -16,6 +16,8 @@ import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
 import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
+import { getOrganization } from './routes/orgs/get-organization'
+import { getOrganizations } from './routes/orgs/get-organizations'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -65,6 +67,8 @@ app.register(resetPassword)
 // orgs
 app.register(createOrganization)
 app.register(getMembership)
+app.register(getOrganization)
+app.register(getOrganizations)
 
 app.listen({ port: env.PORT }).then(() => {
     console.log('HTTP server running!')
