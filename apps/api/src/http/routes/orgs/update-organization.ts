14d22ef6b40220615069ef5cb5ae3993b7689dd7
csendin <caiosendindev@gmail.com>
2025-01-28 19:53:05 -0300

    feat: add organization routes

---
+++ apps/api/src/http/routes/orgs/update-organization.ts
@@ -0,0 +1,77 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { BadRequestError } from '@/http/errors/bad-request-error'
+import { UnauthorizedError } from '@/http/errors/unauthorized-error'
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+import { organizationSchema } from '@saas/auth'
+
+export async function updateOrganization(app: FastifyInstance) {
+    app.withTypeProvider<ZodTypeProvider>()
+        .register(auth)
+        .put(
+            '/organizations/:slug',
+            {
+                schema: {
+                    tags: ['organizations'],
+                    summary: 'Update organization details',
+                    security: [{ bearerAuth: [] }],
+                    body: z.object({
+                        name: z.string(),
+                        domain: z.string().nullish(),
+                        attachUsersByDomain: z.boolean(),
+                    }),
+                    params: z.object({
+                        slug: z.string(),
+                    }),
+                    response: {
+                        204: z.null(),
+                    },
+                },
+            },
+            async (request, reply) => {
+                const { slug } = request.params
+
+                const userId = await request.getCurrentUserId()
+
+                const { membership, organization } = await request.getUserMembership(slug)
+
+                const { name, domain, attachUsersByDomain } = request.body
+
+                const authOrganization = organizationSchema.parse(organization)
+
+                const { cannot } = getUserPermissions(userId, membership.role)
+
+                if (cannot('update', authOrganization)) {
+                    throw new UnauthorizedError(`You're not allowed to update this organization.`)
+                }
+
+                if (domain) {
+                    const organizationByDomain = await prisma.organization.findFirst({
+                        where: {
+                            domain,
+                            id: { not: organization.id },
+                        },
+                    })
+
+                    if (organizationByDomain) {
+                        throw new BadRequestError('Another organization with same domain already exists.')
+                    }
+                }
+
+                await prisma.organization.update({
+                    where: { id: organization.id },
+                    data: {
+                        name,
+                        domain,
+                        attachUsersByDomain,
+                    },
+                })
+
+                return reply.status(204).send()
+            }
+        )
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,6 +18,9 @@ import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
 import { getOrganization } from './routes/orgs/get-organization'
 import { getOrganizations } from './routes/orgs/get-organizations'
+import { shutdownOrganization } from './routes/orgs/shutdown-organization'
+import { transferOrganization } from './routes/orgs/transfer-organization'
+import { updateOrganization } from './routes/orgs/update-organization'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -69,6 +72,9 @@ app.register(createOrganization)
 app.register(getMembership)
 app.register(getOrganization)
 app.register(getOrganizations)
+app.register(shutdownOrganization)
+app.register(transferOrganization)
+app.register(updateOrganization)
 
 app.listen({ port: env.PORT }).then(() => {
     console.log('HTTP server running!')
