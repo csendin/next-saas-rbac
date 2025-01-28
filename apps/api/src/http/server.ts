6d80d866a2e0b72191e2fde30789fe1c380731e5
csendin <caiosendindev@gmail.com>
2025-01-28 20:09:46 -0300

    feat: add project routes

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -21,6 +21,11 @@ import { getOrganizations } from './routes/orgs/get-organizations'
 import { shutdownOrganization } from './routes/orgs/shutdown-organization'
 import { transferOrganization } from './routes/orgs/transfer-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
+import { createProject } from './routes/projects/create-project'
+import { deleteProject } from './routes/projects/delete-project'
+import { getProject } from './routes/projects/get-project'
+import { getProjects } from './routes/projects/get-projects'
+import { updateProject } from './routes/projects/update-project'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -75,6 +80,13 @@ app.register(getOrganizations)
 app.register(shutdownOrganization)
 app.register(transferOrganization)
 app.register(updateOrganization)
+
+// projects
+app.register(createProject)
+app.register(deleteProject)
+app.register(getProject)
+app.register(getProjects)
+app.register(updateProject)
 
 app.listen({ port: env.PORT }).then(() => {
     console.log('HTTP server running!')
