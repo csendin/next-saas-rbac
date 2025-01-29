b21af65c5823d430b5cc111576fec0007537c36b
csendin <caiosendindev@gmail.com>
2025-01-28 21:06:05 -0300

    feat: add invite routes

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -14,7 +14,16 @@ import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
+import { acceptInvite } from './routes/invites/accept-invite'
+import { createInvite } from './routes/invites/create-invite'
+import { getInvite } from './routes/invites/get-invite'
+import { getInvites } from './routes/invites/get-invites'
+import { getPendingInvites } from './routes/invites/get-pending-invites'
+import { rejectInvite } from './routes/invites/reject-invite'
+import { revokeInvite } from './routes/invites/revoke-invite'
 import { getMembers } from './routes/members/get-members'
+import { removeMember } from './routes/members/remove-member'
+import { updateMember } from './routes/members/update-member'
 import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
 import { getOrganization } from './routes/orgs/get-organization'
@@ -91,6 +100,17 @@ app.register(updateProject)
 
 // members
 app.register(getMembers)
+app.register(removeMember)
+app.register(updateMember)
+
+// invites
+app.register(acceptInvite)
+app.register(createInvite)
+app.register(getInvite)
+app.register(getInvites)
+app.register(getPendingInvites)
+app.register(rejectInvite)
+app.register(revokeInvite)
 
 app.listen({ port: env.PORT }).then(() => {
     console.log('HTTP server running!')
